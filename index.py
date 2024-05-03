b5e9df213037368ba99e323204ff4e9e635c1f9a
ze5h4an5sx <168797290+ze5h4an5sx@users.noreply.github.com>
2024-05-03 11:16:12 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,38 @@
+# 2024/5/3
+import os
+
+import requests
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+host = os.environ.get("host")
+
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'HEAD', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'HEAD', 'DELETE'])
+def proxy(path):
+    if not host:
+        return '请先设置 host'
+    
+    url = f'{host}/{path}'  # 替换为目标地址
+    headers = dict(request.headers)
+    headers.pop('Host')
+    params = dict(request.args)
+    method = request.method
+
+    if method == 'GET':
+        response = requests.get(url, headers=headers, params=params)
+    elif method == 'POST':
+        response = requests.post(url, headers=headers, params=params, data=request.data)
+    elif method == 'HEAD':
+        response = requests.head(url, headers=headers, params=params)
+    elif method == 'DELETE':
+        response = requests.delete(url, headers=headers, params=params)
+
+    response = Response(response.content, status=response.status_code, headers=dict(response.headers.items()))
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
